d1a1365ed2e8356d7bdb147e5f2cc37421c86b98
twooopark <twooo.park@gmail.com>
2018-06-19 13:42:59 +0900

    Get html Code

--- idontknow.py
+++ idontknow.py
@@ -1,6 +1,10 @@
 from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 
 request = Request('http://movie.naver.com/movie/sdb/rank/rmovie.nhn')
 response = urlopen(request)
 html = response.read().decode('cp949')
 print(html)
+
+bs = BeautifulSoup(html, 'html.parser')
+print(bs.prettify())
