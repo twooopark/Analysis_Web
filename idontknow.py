0140e2bc892de0a4048f6362c6b2fd1dc4abf4f1
twooopark <twooo.park@gmail.com>
2018-06-19 13:55:28 +0900

    Get movie rank, movie

--- idontknow.py
+++ idontknow.py
@@ -4,7 +4,17 @@ from bs4 import BeautifulSoup
 request = Request('http://movie.naver.com/movie/sdb/rank/rmovie.nhn')
 response = urlopen(request)
 html = response.read().decode('cp949')
-print(html)
+# print(html)
 
 bs = BeautifulSoup(html, 'html.parser')
-print(bs.prettify())
+# print(bs.prettify())
+
+tags = bs.findAll('div', attrs={'class': 'tit3'})
+
+# a태그 안에 내용(text)
+# for tag in tags:
+#     print(tag.a.text)
+
+# + 넘버링
+for index, tag in enumerate(tags):
+    print(index, tag.a.text, tag.a['href'], sep=' : ')
