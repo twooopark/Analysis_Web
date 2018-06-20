34ac129e669fee51ab4e586bce69be061ca9740a
twooopark <twooo.park@gmail.com>
2018-06-20 10:22:45 +0900

    lambda 활용 예시

--- __main__.py
+++ __main__.py
@@ -3,15 +3,16 @@ import collection
 # import visualize
 # from config import CONFIG
 
-# 각 모듈의 에러 메세지를 받아와서 메인에서 출력하도록 한다.
-def my_error(e):
-    print("myerror: " + str(e))
+# 처리하는 함수를 만들어서, 내부에서 처리하게끔 해보자
+def proc(html):
+    print("processing..."+html)
 
+def store(result):
+    pass
 
 if __name__ == '__main__':
     result = collection.crawling(
-                url='http://movie.naver.com/movie/sdb/rank/rmovie.nh1n',
-                encoding='cp949')
-                # err=my_error)
-                # 원래 기본대로 내부에서 처리하는게 낫다.
-    print(result)
+                url='http://movie.naver.com/movie/sdb/rank/rmovie.nhn',
+                encoding='cp949',
+                proc=proc,
+                store=store)
--- collection/crawler.py
+++ collection/crawler.py
@@ -5,14 +5,20 @@ import sys
 def crawling(
         url='',
         encoding='utf-8',
-        err=lambda e: print('%s : %s' % (e, datetime.now()), file=sys.stderr)):
+        err=lambda e: print('%s : %s' % (e, datetime.now()), file=sys.stderr),
+        proc=lambda html: html, # 전달된 함수가 없으면, 그냥 넘어감
+        store=lambda html: html):# lambda html: print("processing..."+html)):
     try:
         request = Request(url)
         response = urlopen(request)
         try:
             receive = response.read()
             # 인코딩 방식이 다양하면 어떻게 해야할까 ?
-            result = receive.decode(encoding)
+            result = store(proc(receive.decode(encoding)))
+            # if proc is not None: -->  proc=lambda html: html, 를 함으로써 생략가능해짐
+            #         result = proc(result)
+            # if store is not None: --> 위와 마찬가지로 생략
+            #         result = store(result)
         except UnicodeDecodeError:
             # Legal values for this argument are
             # 'strict' (raise a UnicodeDecodeError exception),
